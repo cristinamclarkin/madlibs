3631434756e8add8b7267db5c7ae9fa51c118a92
Hackbright Student <info@hackbrightacademy.com>
2016-04-19 17:34:38 -0700

    completed madlibs excersize

--- madlibs.py
+++ madlibs.py
@@ -38,6 +38,25 @@ def greet_person():
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_game_form():
+    """Routes to game form"""
+    answer = request.args.get("game")
+    player = request.args.get("person")
+    
+    if answer == "yes":
+        return render_template("game.html", person=player)
+    else:
+        return render_template("goodbye.html", person=player)
+
+@app.route('/madlibs')
+def show_madlib():
+    """ """
+    name = request.args.get('myname')
+    color = request.args.get('color')
+    noun = request.args.get('noun')
+
+    return render_template("madlibs.html", name=name, color=color, noun=noun)
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
